1ffc7ded742fbe3f11ea3eeafeb5f43277020aa6
Quentin Legot <legotquentin@gmail.com>
2021-03-27 15:05:01 +0100

    Fix Random.nextInt(int) in Random.java

--- src/battleship/model/player/Random.java
+++ src/battleship/model/player/Random.java
@@ -2,12 +2,12 @@ package battleship.model.player;
 
 import battleship.utils.Pair;
 
-public class Random extends Player{
+public class Random extends Player {
 
     @Override
     public Pair<Integer,Integer> chooseMove() {
-        Random rand = new Random();
-        int index  = validMoves().get(rand.nextInt(validMoves().size()));
-        return validMoves().get(index);
+        java.util.Random rand = new java.util.Random();
+        // Pair<Integer, Integer> index  = validMoves().get(rand.nextInt(validMoves().size()));
+        return validMoves().get(rand.nextInt(validMoves().size()));
     }
 }
